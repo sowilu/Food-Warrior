4e3775e63025c1a4d20d8ca8af8f6b7f8c8e2ace
sowilu <sima.simute1999@gmail.com>
2024-02-08 17:04:15 +0200

    Juicy vfx

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
+    public Color juicesColor;
+    public GameObject juicesVfx;
+    public AudioClip slashSound;
+
     private void OnCollisionEnter(Collision collision)
     {
         //todo: if fruits collide with each other check if sword
         GetComponent<Collider>().enabled = false;
+        GetComponent<TrailRenderer>().enabled = false;
+
+        //spawn juices
+        var juices = Instantiate(juicesVfx, transform.position, Quaternion.identity);
+        var main = juices.GetComponent<ParticleSystem>().main;
+        main.startColor = juicesColor;
+
+        //play splash sound
+        AudioManager.instance.Play(slashSound);
+
+        //spawn splash
+        Splasher.instance.SpawnSplash(juicesColor, transform.position + Vector3.forward * 2);
 
         var dir = Vector3.right;
         foreach(Transform child in transform)
